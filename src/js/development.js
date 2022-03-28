c58d33f4a8527ca63e014a3d92c048c2a0f521f8
Peresvet <Koltyrinpk@gmail.com>
2022-03-28 21:07:10 +0400

    добавлена страница "повышение квалификации"

---
+++ src/js/development.js
@@ -0,0 +1,76 @@
+import '../scss/development.scss';
+$(() => {
+	function dropDownFunc() {
+		let containerDropDown = document.querySelector('body');
+		let dropDown = document.querySelector('.dropdown');
+
+		containerDropDown.addEventListener('click', (event) => {
+			if (event.target.classList.contains('btn-dropdown')) {
+				dropDown.classList.toggle('dropdown-active');
+			}
+			else {
+				dropDown.classList.remove('dropdown-active');
+			}
+		});
+	}
+	function burger() {
+		let burger = document.querySelector(".burger");
+		burger.addEventListener("click", function () {
+			let aside = document.querySelector(".aside");
+			let bod = document.querySelector('.scroll');
+
+			aside.classList.toggle("aside--active");
+			bod.classList.toggle("fixed-scroll");
+			burger.classList.toggle("burger-active");
+		});
+	}
+	$('.tabs__item').click(function () {
+		var id = $(this).attr('data-tab'),
+			content = $('.tab-content[data-tab="' + id + '"]');
+
+		$('.tabs__item.tabs__item--active').removeClass('tabs__item--active'); // 1
+		$(this).addClass('tabs__item--active'); // 2
+
+		$('.tab-content.active').removeClass('active'); // 3
+		content.addClass('active'); // 4
+	});
+	function editCourse() {
+		let edit = document.getElementsByClassName("edit");
+		for (let i = 0; i < edit.length; i++) {
+			edit[i].addEventListener("click", function () {
+				let myLessons = document.querySelector("#my-lessons");
+				let myLessonsEdit = document.querySelector("#my-lessons--edit");
+
+				myLessons.classList.toggle("hide");
+				myLessonsEdit.classList.toggle("active");
+				
+			});
+		}
+	}
+	function goBack(){
+		let edit = document.querySelector(".back-arrow");
+			edit.addEventListener("click", function () {
+				let myLessons = document.querySelector("#my-lessons");
+				let myLessonsEdit = document.querySelector("#my-lessons--edit");
+				myLessons.classList.toggle("hide");
+				myLessonsEdit.classList.toggle("active");	
+			});
+	}
+	function dropDownAside(){
+		let item = document.getElementsByClassName('menu__box--dropdown');
+		for(let i = 0; i < item.length; i++){
+			item[i].addEventListener('click', function(){
+				let arrow = item[i].querySelector(".menu__link--arrow");
+				let dropdown = item[i].querySelector('.aside__dropdown');
+
+				dropdown.classList.toggle('aside__dropdown--active');
+				arrow.classList.toggle('rotate180');
+			});
+		}
+	}
+	dropDownFunc();
+	burger();
+	editCourse();
+	goBack();
+	dropDownAside();
+});
