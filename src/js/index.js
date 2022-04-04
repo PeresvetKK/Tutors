b6696fce60536e73ff1a146f1d2e08acdb6fe466
Peresvet <Koltyrinpk@gmail.com>
2022-04-04 17:36:32 +0400

    x

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,23 @@
 import '../scss/index.scss';
-import {modal, sortTable, mainDropdown, dropDownFunc, burger, tabs, editCourse, changeBtn, profileDropDown, tableDropdown, dropDownAside} from '../vendors/script';
+import AirDatepicker from 'air-datepicker'
+import 'air-datepicker/air-datepicker.css'
+import {tableLoader, modal, sortTable, mainDropdown, dropDownFunc, burger, tabs, editCourse, changeBtn, profileDropDown, tableDropdown, dropDownAside} from '../vendors/script';
 $(() => {
+	var dat = new AirDatepicker('#set-data', {
+		onSelect: function(dataText, inst){
+			var dateAsString = dataText.formattedDate;
+			console.log(dateAsString);
+			var input = document.getElementById('set-data');
+			input.value = dateAsString;
+			input.setAttribute('data-quantity', input.value);
+		}
+	});
+
+	var input = document.getElementById('set-data');
+	input.addEventListener('input', function () {
+		input.setAttribute('data-quantity', input.value);
+	});
+	tableLoader();
 	modal();
 	sortTable();
 	mainDropdown();
--- src/js/my-lessons.js
+++ src/js/my-lessons.js
@@ -1,5 +1,7 @@
 import '../scss/my-lessons.scss';
-import {tableLoader,tabs, sortTable, mainDropdown, dropDownFunc, burger, tableDropdown, dropDownAside} from '../vendors/script';
+import AirDatepicker from 'air-datepicker'
+import 'air-datepicker/air-datepicker.css'
+import {editCourse, tableLoader,tabs, sortTable, mainDropdown, dropDownFunc, burger, tableDropdown, dropDownAside} from '../vendors/script';
 $(() => {
 	tableLoader();
 	tabs();
@@ -8,6 +10,7 @@ $(() => {
 	dropDownFunc();
 	burger();
 	tableDropdown();
+	editCourse();
 	dropDownAside();
 	let edit = document.getElementsByClassName("edit-icon");
 
@@ -24,4 +27,18 @@ $(() => {
 			information.classList.add('tabs__item--active');
 		});
 	}
+	var dat = new AirDatepicker('#set-data', {
+		onSelect: function(dataText, inst){
+			var dateAsString = dataText.formattedDate;
+			console.log(dateAsString);
+			var input = document.getElementById('set-data');
+			input.value = dateAsString;
+			input.setAttribute('data-quantity', input.value);
+		}
+	});
+
+	var input = document.getElementById('set-data');
+	input.addEventListener('input', function () {
+		input.setAttribute('data-quantity', input.value);
+	});
 });
