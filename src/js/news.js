177c470fb2d34109abdd4d8e0dac648bd18e2d48
Peresvet <Koltyrinpk@gmail.com>
2022-04-08 18:02:39 +0400

    добавлена страница "новости"

---
+++ src/js/news.js
@@ -0,0 +1,42 @@
+import '../scss/news.scss';
+import {dropDownFunc, burger, dropDownAside, mainDropdown} from '../vendors/script';
+$(() => {
+	$('.tabs__item').click(function () {
+		var id = $(this).attr('data-tab'),
+			content = $('.tab-content[data-tab="' + id + '"]');
+
+		$('.tabs__item.tabs__item--active').removeClass('tabs__item--active'); // 1
+		$(this).addClass('tabs__item--active'); // 2
+
+		$('.tab-content.active').removeClass('active'); // 3
+		content.addClass('active'); // 4
+	});
+    function editCourse() {
+		let edit = document.getElementsByClassName("edit");
+		for (let i = 0; i < edit.length; i++) {
+			edit[i].addEventListener("click", function () {
+				let myLessons = document.querySelector("#my-lessons");
+				let myLessonsEdit = document.querySelector("#my-lessons--edit");
+
+				myLessons.classList.toggle("hide");
+				myLessonsEdit.classList.toggle("active");
+				
+			});
+		}
+	}
+    function goBack(){
+		let edit = document.querySelector(".back-arrow");
+			edit.addEventListener("click", function () {
+				let myLessons = document.querySelector("#my-lessons");
+				let myLessonsEdit = document.querySelector("#my-lessons--edit");
+				myLessons.classList.toggle("hide");
+				myLessonsEdit.classList.toggle("active");	
+			});
+	}
+    goBack();
+    editCourse();
+	mainDropdown();
+	dropDownFunc();
+	burger();
+	dropDownAside();
+});
