b279144d545a23eeb2039ac821cb9aabb6645a36
Peresvet <Koltyrinpk@gmail.com>
2022-05-31 13:52:03 +0400

    страница авторизации

--- src/js/login.js
+++ src/js/login.js
@@ -6,12 +6,7 @@ import 'clockpicker/src/clockpicker.js';
 import 'clockpicker/src/clockpicker.css';
 import {longMessage, sendScroll, checkKey, dinamocBtn, offset, breadsDrop, pushClose, openNotify,	setDate, setTime, tableLoader, modal, sortTable, mainDropdown, dropDownFunc, burger, tabs, editCourse, changeBtn, tableDropdown, dropDownAside, editCourseTwo, openExit} from '../vendors/script';
 $(() => {
-	setDate('date1');
-	setDate('date2');
-	setDate('date3');
-	setDate('date10');
-	setDate('date11');
-	setTime('settime1');
+
 	function modalEvent() {
 		let btn = document.getElementsByClassName('modalevent');
 		for (let i = 0; i < btn.length; i++) {
@@ -48,71 +43,43 @@ $(() => {
 	sortTable();
 	mainDropdown();
 	dropDownFunc();
-	burger();
 	tabs();
 	// editCourse();
 	longMessage();
-	openExit();
-	editCourseTwo("course-table__id", "#my-lessons--edit");
-	editCourseTwo("open-lesson", "#estimation");
-	editCourseTwo("open-dogovor", "#deals");
 	changeBtn();
 	tableDropdown();
 	dropDownAside();
 	pushClose();
 	dinamocBtn();
 	sendScroll();
-	function pencelCourse() {
-		let edit = document.getElementsByClassName("edit-block-cell__inner");
-		for (let i = 0; i < edit.length; i++) {
-			edit[i].addEventListener("click", function () {
-				let myLessons = document.querySelector("#my-lessons");
-				let myLessonsEdit = document.querySelector("#my-lessons--edit");
-				let pancelInner = document.querySelector('#estimation');
-				let esti = document.querySelector('#estimationItem');
-				let estiblock = pancelInner.querySelector("#open");
-				// myLessons.classList.toggle("hide");
+
+    $('.recovery-btn').on('click', function(){
+        let myLessons = document.querySelector(".recovery");
+				let myLessonsEdit = document.querySelector(".login");
+				myLessons.classList.toggle("hide");
+				myLessonsEdit.classList.toggle("active");	
+    });
+    $('.open-push').on('click', function(){
+        let myLessons = document.querySelector(".recovery");
+				let myLessonsEdit = document.querySelector(".push");
+				myLessons.classList.toggle("hide");
+				myLessonsEdit.classList.toggle("active");	
+    })
+    $('.go-login').on('click', function(){
+        let myLessons = document.querySelector(".push");
+				let myLessonsEdit = document.querySelector(".login");
+				myLessons.classList.toggle("active");
 				myLessonsEdit.classList.toggle("active");
-				pancelInner.classList.toggle('active');
-				esti.classList.toggle('tabs__item--active');
-				estiblock.classList.toggle('active');
+    });
+    function goBack(){
+		let edit = document.querySelector(".back");
+			edit.addEventListener("click", function () {
+				let myLessons = document.querySelector(".recovery");
+				let myLessonsEdit = document.querySelector(".login");
+				myLessons.classList.toggle("hide");
+				myLessonsEdit.classList.toggle("active");	
 			});
-		}
 	}
-	function breadsmodal(){
-		
-			let edit = document.getElementsByClassName("open-block");
-			for (let i = 0; i < edit.length; i++) {
-				edit[i].addEventListener("click", function () {
-					let myLessons = document.querySelector("#my-lessons");
-					let deals = document.querySelector('#deals');
-					let estiblock = document.querySelector("#open-tab");
-					let esti = document.querySelector('#button-active');
-					myLessons.classList.toggle("hide");
-					deals.classList.toggle('active');
-					estiblock.classList.toggle('active');
-					esti.classList.toggle('tabs__item--active');
-				});
-			}
-	}
-	pencelCourse();
-	breadsmodal();
-	function modalDogovor(){
-		let btn = document.getElementsByClassName('btn-dogovor');
-		for (let i = 0; i < btn.length; i++) {
-			btn[i].addEventListener('click', function () {
-				let mainbody = document.querySelector('.scroll');
-				mainbody.classList.add('fixed-scroll');
-				let modal = document.querySelector('.modal-dogovor');
-				let close = modal.querySelector('.modal__inner-svg');
-				modal.classList.toggle('active__modal');
-				close.addEventListener('click', function () {
-					modal.classList.remove('active__modal');
-					mainbody.classList.remove('fixed-scroll');
-				});
-			});
-		}
-	}
-	modalDogovor();
+    goBack();
 	// stars('stars__input');
 });
