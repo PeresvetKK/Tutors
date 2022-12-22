e2b3c1d61aa748476001ece7f390c9a7ce14449b
PeresvetKK <koltyrin2309@gmail.com>
2022-12-22 23:22:28 +0400

    сохраненные фильтры

--- src/js/free-courses.js
+++ src/js/free-courses.js
@@ -94,9 +94,96 @@ $(() => {
 	// dateSlider.noUiSlider.on('update', function (values, handle) {
 	// 	dateValues[handle].innerHTML = formatter.format(new Date(+values[handle]));
 	// });
-	
-	timeSlider(".slider-one");
-	timeSlider(".slider-two");
-	timeSlider(".slider-sixtin");
-	timeSlider(".slider-fourtin");
+	// timeSlider(".slider-one");
+	// timeSlider(".slider-two");
+	// timeSlider(".slider-sixtin");
+	// timeSlider(".slider-fourtin");
+
+
+	// дроп "готовые фильтры"
+	const dropwDowns = (triger, trigered) =>{
+		document.querySelector(`.${triger}`).addEventListener('click', (e) => e.currentTarget.querySelector(`.${trigered}`)
+																			   .classList.toggle('active'));
+	}
+	// при клике на "сохранить фильтры" в модалке добавление фильтров(доделать), удаление фильтра, отмена фильтров
+	const filtresModal = () => {
+		$('.save-filter-btn').on('click', (e) => {
+			// let input = e.target.parentElement.firstElementChild.nextElementSibling.firstElementChild.firstElementChild;
+			let categories = document.querySelector('.modal-saveFiltres').querySelector('.filtres-names')
+			let modal = $('.modal-saveFiltres .filtres-names');
+			categories.insertAdjacentHTML('beforeend', 
+				`
+				<button class="filtres-name" type="button">
+					${'input.value'}
+					<svg class="removeFiltresCategory" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M16 16L8 8" stroke="#8F9498" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+						<path d="M16 8L8 16" stroke="#8F9498" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+					</svg>                                        
+				</button>	
+				`)
+				// input.value = '' 
+			
+			
+			$('.cancel-filtres').on('click', (e) => {
+				e.target.parentElement.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling.innerHTML = ""
+			})
+			$('.removeFiltresCategory').on('click', (e) => {
+				e.currentTarget.parentElement.remove();
+			})
+		})
+		$('.removeFiltresCategory').on('click', (e) => {
+			e.currentTarget.parentElement.remove();
+		})
+	}
+	// добавляет в таблицу строку с фильтрами
+	const addRowCompitedFiltres = () => {
+		$('.complited-filtres').on('click', (e) => {
+			e.target.parentElement.parentElement.parentElement.parentElement.parentElement.nextElementSibling.classList.add('created-filtres-row--active')
+			e.target.parentElement.parentElement.parentElement.parentElement.parentElement.nextElementSibling.insertAdjacentHTML('afterbegin', 
+			`
+			<button class="filtres-name row-filtres-name" type="button">
+				input.value
+				<svg class="removeFiltresCategory" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M16 16L8 8" stroke="#8F9498" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+					<path d="M16 8L8 16" stroke="#8F9498" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+				</svg>                                        
+			</button>
+			<button class="filtres-name row-filtres-name" type="button">
+				input.value
+				<svg class="removeFiltresCategory" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M16 16L8 8" stroke="#8F9498" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+					<path d="M16 8L8 16" stroke="#8F9498" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+				</svg>                                        
+			</button>
+			<button class="filtres-name row-filtres-name" type="button">
+				input.value
+				<svg class="removeFiltresCategory" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M16 16L8 8" stroke="#8F9498" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+					<path d="M16 8L8 16" stroke="#8F9498" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+				</svg>                                        
+			</button>
+			`
+			)	
+			// e.target.parentElement.parentElement.parentElement.parentElement.parentElement.insertAdjacentHTML('afterend', 
+			// `
+
+			// `
+			// )	
+			$('.reset-compited-filtres').on('click', (e) => {
+				e.target.parentElement.classList.remove('created-filtres-row--active')
+			})
+			$('.row-filtres-name').on('click', (e) =>{
+				e.currentTarget.remove();
+			})
+		})
+	}
+
+	addRowCompitedFiltres();
+	// при клике на "сохранить фильтры" открывается модалка
+	modalNew('save-filter-btn', 'modal-saveFiltres');
+	// открывается дроп "готовые фильтры"
+	dropwDowns('filtres-drop', 'filtres-save-drop');
+	// при клике на карандаш открывается модалка редактирования фильтра
+	modalNew('filtres-save-drop-1', 'modal-complited-filtres')
+	filtresModal();
 });
